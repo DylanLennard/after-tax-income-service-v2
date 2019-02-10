65b90cb99ad6e462e90a8702565ca0aef094d593
DylanLennard <dlennard09@yahoo.com>
2019-02-09 16:12:52 -0800

    02/09/19 4:11 PM || minor adjustments to varnames in helpers and ordering of funcs in handler.go

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,14 +1,14 @@
 package helpers
 
 // This function calculates SSI and medicare related taxes
-func CalculateOtherTaxes(income float64, status bool) float64{
-	
+func CalculateOtherTaxes(income float64, selfEmpStatus bool) float64{
+
     medicare_rate := 0.0145
     medicare_upper_rate := 0.0235 // for those making over $200000
 	ssi_rate := 0.062 //up to $118,500
 	var total_other_tax, ssi_tax, medicare_tax float64
 
-    if !status{
+    if selfEmpStatus{
         medicare_rate = medicare_rate * 2
         medicare_upper_rate = medicare_upper_rate * 2
         ssi_rate = ssi_rate * 2
--- service/handler.go
+++ service/handler.go
@@ -14,29 +14,34 @@ func HelloWorld(w http.ResponseWriter, req *http.Request, params httprouter.Para
 }
 
 // This handler results in the calculation of your after tax income
-// assuming you live in the state of california for the
-// 2018 tax year. 
+// assuming you live in the state of california for the 2018 tax year. 
 // It'll eventually take in the URL and parse out your income and status level 
 // and pass that into the Tax function defined in helpers  
 // AFter that it calculates federal, state, and other taxes
 // and subtracts them from your income and returns the final income
 func AfterTaxIncome(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 
-	// TODO: handle request
-	// parse out income and hold status static for now
 	queryVals := req.URL.Query()
+
+	// income 
 	incomeStr := queryVals.Get("income")
 	income, err := strconv.ParseFloat(incomeStr, 64)
-
 	if err != nil { 
 		fmt.Fprint(w, "Cannot process income parameter: ", incomeStr)
 		return
 	}
 
-	// Get the various tax values 
+	// employment status
+	selfEmpStatusStr := queryVals.Get("status")
+	selfEmpStatus, err_new := strconv.ParseBool(selfEmpStatusStr)
+	if err_new != nil{
+		selfEmpStatus = false
+	}
+
+	// Get the various tax values
 	federalTax := FederalTaxes.Calculate(income)
 	stateTax := StateTaxes.Calculate(income)
-	otherTax := helpers.CalculateOtherTaxes(income, true)
+	otherTax := helpers.CalculateOtherTaxes(income, selfEmpStatus)
 	afterTax := income - (federalTax + stateTax + otherTax)
 	
 	// write to response
