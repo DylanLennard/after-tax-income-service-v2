1d9f56020020fba6f6dfda9e93a91dc1aec81da4
DylanLennard <dlennard09@yahoo.com>
2019-02-14 05:24:15 -0800

    02/14/19 05:23 AM || changed var names to not be underscore for CalculateOtherTaxes

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,30 +1,67 @@
 package helpers
 
-// This function calculates SSI and medicare related taxes
-func CalculateOtherTaxes(income float64, selfEmpStatus bool) float64{
+import (
+	"github.com/DylanLennard/after-tax-income-service-v2/model"
+)
 
-    medicare_rate := 0.0145
-    medicare_upper_rate := 0.0235 // for those making over $200000
-	ssi_rate := 0.062 //up to $118,500
-	var total_other_tax, ssi_tax, medicare_tax float64
+// CalculateOtherTaxes calculates SSI and medicare related taxes
+func CalculateOtherTaxes(income float64, selfEmpStatus bool) float64 {
 
-    if selfEmpStatus{
-        medicare_rate = medicare_rate * 2
-        medicare_upper_rate = medicare_upper_rate * 2
-        ssi_rate = ssi_rate * 2
+	medicareRate := 0.0145
+	medicareUpperRate := 0.0235 // for those making over $200000
+	ssiRate := 0.062            //up to $118,500
+	var totalOtherTax, ssiTax, medicareTax float64
+
+	if selfEmpStatus {
+		medicareRate = medicareRate * 2
+		medicareUpperRate = medicareUpperRate * 2
+		ssiRate = ssiRate * 2
 	}
 
-    if income < 118500 {
-        ssi_tax = income * ssi_rate
-		medicare_tax = income * medicare_rate
+	if income < 118500 {
+		ssiTax = income * ssiRate
+		medicareTax = income * medicareRate
 	} else {
-        ssi_tax = 118500 * ssi_rate
-        if income <= 200000{
-			medicare_tax = income * medicare_rate
-		} else{
-			medicare_tax = income * medicare_upper_rate
+		ssiTax = 118500 * ssiRate
+		if income <= 200000 {
+			medicareTax = income * medicareRate
+		} else {
+			medicareTax = income * medicareUpperRate
 		}
 	}
-    total_other_tax = ssi_tax + medicare_tax
-    return total_other_tax
+	totalOtherTax = ssiTax + medicareTax
+	return totalOtherTax
+}
+
+// GetTaxInfo gets all the full data
+func GetTaxInfo() (model.Taxes, model.Taxes) {
+
+	// open the two files and unmarshal the JSON appropriately
+	// fedTaxFile, errFed := os.Open("users.json")
+	// defer fedTaxFile.close()
+	// if errFed != nil {
+	// 	fmt.Println(errFed)
+	// }
+	// stateTaxFile, errState := os.Open("users.json")
+	// defer stateTaxFile.close()
+	// if errState != nil {
+	// 	fmt.Println(errState)
+	// }
+
+	//unmarshal the JSON
+
+	// TODO: delete this once you've gotten data from file explicitly
+	StateTaxes := model.Taxes{
+		Rates: []float64{0.01, 0.02, 0.04, 0.06, 0.08, 0.093,
+			0.103, 0.113, 0.123, 0.133},
+		Brackets: []float64{0, 8015, 19001, 29989, 41629, 52612, 268750,
+			322499, 537498, 1000000},
+	}
+	FederalTaxes := model.Taxes{
+		Rates:    []float64{0.1, 0.12, 0.22, 0.24, 0.32, 0.35, 0.37},
+		Brackets: []float64{0, 9526, 38701, 82501, 157501, 200001, 500001},
+	}
+
+	return FederalTaxes, StateTaxes
+
 }
